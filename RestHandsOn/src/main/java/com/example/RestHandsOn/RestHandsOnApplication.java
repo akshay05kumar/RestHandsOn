645a065bbbf6b312db8d5f8a8949355cd1ecaabf
Akshay Kumar <kumarakshay@microsoft.com>
2024-12-10 22:10:02 +0530

    Setting up a springboot application

---
+++ RestHandsOn/src/main/java/com/example/RestHandsOn/RestHandsOnApplication.java
@@ -0,0 +1,13 @@
+package com.example.RestHandsOn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestHandsOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestHandsOnApplication.class, args);
+	}
+
+}
---
+++ RestHandsOn/src/test/java/com/example/RestHandsOn/RestHandsOnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RestHandsOn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestHandsOnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
